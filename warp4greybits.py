0e8f2ad0c3f7f1ae6a7be7fc929ba8e92f20d362
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2021-07-29 20:49:11 +0100

    new file:   ../warp4greybits.py 	modified:   ../warping.py

--- warp4greybits.py
+++ warp4greybits.py
@@ -42,9 +42,14 @@ if 0 ==0:
  
  
     # FIND THE BIGGEST COUNTOUR
+    # Sort the contours in terms of size
+    sortedcontours = sorted(contours, key=cv2.contourArea, reverse=True)
+    peri = cv2.arcLength(sortedcontours[2], True)
+    approx = cv2.approxPolyDP(sortedcontours[2], 0.02 * peri, True)
+
     biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
     if biggest.size != 0:
-        biggest=utlis.reorder(biggest)
+        biggest=utlis.reorder(approx)
         cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
         imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
         pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
@@ -55,7 +60,23 @@ if 0 ==0:
         #REMOVE 20 PIXELS FORM EACH SIDE
         imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
         imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
- 
+
+        for i in [0,1,2,3]:
+            peri = cv2.arcLength(sortedcontours[i], True)
+            approx = cv2.approxPolyDP(sortedcontours[i], 0.02 * peri, True)
+            biggest=utlis.reorder(approx)
+            cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
+            imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
+            pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+            pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+            imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+            imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+            cv2.imwrite("Scanned/myImage"+str(i)+".jpg",imgWarpColored)
+            
+
+
         # APPLY ADAPTIVE THRESHOLD
         imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
         imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
