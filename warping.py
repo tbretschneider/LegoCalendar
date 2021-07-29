2badd1f61b23997f6cfbe45663837d96bc2942f5
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2021-07-29 18:36:58 +0100

    modified:   warping.py

--- warping.py
+++ warping.py
@@ -77,12 +77,12 @@ if 0 ==0:
     cv2.imshow("Result",stackedImage)
  
     # SAVE IMAGE WHEN 's' key is pressed
-    if cv2.waitKey(10000) & 0xFF == ord('s'):
+    if cv2.waitKey() & 0xFF == ord('s'):
         cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
         cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
                       (1100, 350), (0, 255, 0), cv2.FILLED)
         cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
                     cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
         cv2.imshow('Result', stackedImage)
-        cv2.waitKey(300)
+        cv2.waitKey()
         count += 1
